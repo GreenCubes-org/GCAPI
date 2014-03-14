fc7e8357016ec6d7e4306df4d47122c7e26d3130
Kern0 <localhost@kern0.ru>
2014-03-14 04:09:04 +0400

    Changed auth to GC-based

--- config/passport.js
+++ config/passport.js
@@ -1,61 +1,117 @@
+//
+/** Passport stuff **/
+//
 var passport = require('passport'),
-    BearerStrategy = require('passport-http-bearer').Strategy,
-    BasicStrategy = require('passport-http').BasicStrategy,
-    LocalStrategy = require('passport-local').Strategy,
-    ClientPasswordStrategy = require('passport-oauth2-client-password').Strategy;
+	BearerStrategy = require('passport-http-bearer').Strategy,
+	BasicStrategy = require('passport-http').BasicStrategy,
+	LocalStrategy = require('passport-local').Strategy,
+	ClientPasswordStrategy = require('passport-oauth2-client-password').Strategy,
+	mysql = require('mysql'),
+	crypto = require('crypto');
 
+//FIXME: Поменять на глобальную переменную
+var cfg = require('./local');
 
-    passport.serializeUser(function(user, done) {
-      done(null, user.id);
-    });
+function handleGCDBDisconnect() {
+	gcdbconn = require('mysql').createConnection({
+		host: cfg.gcdb.host,
+		database: cfg.gcdb.database,
+		user: cfg.gcdb.user,
+		password: cfg.gcdb.password
+	});
+	gcdbconn.connect(function (err) {
+		if (err) {
+			setTimeout(handleGCDBDisconnect, 1000);
+		}
+	});
 
-    passport.deserializeUser(function(id, done) {
-      User.findOne({id:id}, function (err, user) {
-        done(err, user);
-      });
-    });
+	gcdbconn.on('error', function (err) {
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleGCDBDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
 
-    /**
-     * LocalStrategy
-     *
-     * This strategy is used to authenticate users based on a username and password.
-     * Anytime a request is made to authorize an application, we must ensure that
-     * a user is logged in before asking them to approve the request.
-     */
-     passport.use(
-     new LocalStrategy(
+handleGCDBDisconnect();
 
-     function (username, password, done) {
-         process.nextTick(
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
 
-         function () {
-             User.findOne({
-                 name: username
-             }).done(
+passport.deserializeUser(function (id, done) {
+	gcdbconn.query('SELECT login, password FROM users WHERE id = ?', [id], function (err, result, fields) {
+		if (err) return done(err);
 
-             function (err, user) {
-                 if (err) {
-                     console.log(err);
-                     return;
-                 }
+		done(null, {
+			id: id,
+			username: result[0].login,
+			password: result[0].password
+		});
+	});
+});
 
-                 if (!user) {
-                     return done(
-                     null, false, {
-                         message: 'Unknown user ' + username
-                     });
-                 }
+/* GC.DB Auth */
+passport.use(new LocalStrategy(function (username, password, done) {
+	process.nextTick(
+	function () {
+		username = username.replace(/[^a-zA-Z0-9_-]/g, '');
+		gcdbconn.query('SELECT id, password, activation_code FROM users WHERE login = ?', [username], function (err, result) {
+			// database error
+			if (err) {
+				return done(err, false, {
+					message: 'Ошибка базы данных'
+				});
+				// username not found
+			} else if (result.length === 0) {
+				return done(null, false, {
+					message: 'Неверный логин/пароль'
+				});
+				// check password
+			} else if (result[0].activation_code === undefined) {
+				return done(null, false, {
+					message: 'Аккаунт не активирован'
+				});
+			} else {
+				var passwd = result[0].password.split('$');
+				var hash;
+				if (passwd.length == 1) {
+					hash = crypto.createHash('md5')
+						.update(password)
+						.digest('hex');
+				} else {
+					hash = crypto.createHash('sha1')
+						.update(passwd[1] + password)
+						.digest('hex');
+				}
+				// if md5 passwords match
+				if (passwd.length === 1 && passwd[0] === hash) {
+					var user = {
+						id: result[0].id,
+						username: username,
+						password: hash
+					};
+					// if sha1 passwords match
+				} else if (passwd.length !== 1 && passwd[2] === hash) {
+					var user = {
+						id: result[0].id,
+						username: username,
+						password: hash
+					};
 
-                 if (user.password != password) {
-                     return done(
-                     null, false, {
-                         message: 'Invalid password'
-                     });
-                 }
-                 return done(null, user);
-             })
-         });
-     }));
+				} else {
+					return done(null, false, {
+						message: 'Неверный пароль'
+					});
+				}
+
+				done(null, user);
+			}
+		});
+	});
+}));
+
 
 /**
  * BasicStrategy & ClientPasswordStrategy
