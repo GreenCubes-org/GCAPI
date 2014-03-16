51acaeefdd41525ace53efb5b780bf5a5f493e24
Kern0 <localhost@kern0.ru>
2014-03-16 05:01:38 +0400

    Added missing urls to index

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -9,9 +9,12 @@ module.exports = {
 
 	index: function (req, res) {
 		res.json({
-			main_economy_url: 'https://api.greencubes.org/main/economy',
 			user_url: 'https://api.greencubes.org/users/{user}',
 			current_user_url: 'https://api.greencubes.org/user',
+			main_status_url: 'https://api.greencubes.org/main/status',
+			main_economy_url: 'https://api.greencubes.org/main/economy',
+			rpg_status_url: 'https://api.greencubes.org/rpg/status',
+			apocalyptic_status_url: 'https://api.greencubes.org/apocalyptic/status',
 			api_info_url: 'https://api.greencubes.org/api/info',
 			documentation_url: sails.docs_url
 		});
