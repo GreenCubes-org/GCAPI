1290e85541930b6c38b04b2223f7576e71e0f58c
Kern0 <localhost@kern0.ru>
2014-04-05 23:15:33 +0400

    Scripts: add npm install

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,5 @@ cd ..
 
 git pull origin master
 
+npm install
 forever -a -l ../logs/forever.log -o ../logs/out.log -e ../logs/err.log restart gcapi.js
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+npm install
 forever start -a -l ../logs/forever.log -o ../logs/out.log -e ../logs/err.log ../gcapi.sh
