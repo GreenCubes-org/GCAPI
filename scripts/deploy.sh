0766242d760734bc587f8039bda19846393e87a3
Kern0 <localhost@kern0.ru>
2014-04-20 19:49:00 +0400

    Deploy script: Add git reset

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-git pull origin production
+git fetch origin production
+git reset --hard origin/master
 
 npm install
 forever -a -l ../logs/forever.log -o ../logs/out.log -e ../logs/err.log restart gcapi.js
