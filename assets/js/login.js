55063f54f34e567e3f62bfcf714ffeb9f763d9c7
Kern0 <localhost@kern0.ru>
2014-04-25 21:33:47 +0400

    Assets/login.js: Add window resizing

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,18 @@
 $(function () {
+	function resizeWindow(onResize) {
+		var wndH = window.outerHeight - window.innerHeight;
+		wndH = (!wndH || wndH < 0) ? 50 : wndH;
+		var wndW = window.outerWidth - window.innerWidth;
+		wndW = (!wndW || wndW < 0) ? 50 : wndW;
+
+		var ctrlH = 500 + wndH;
+		var ctrlW = 605 + (wndW || 15);
+
+		window.resizeTo(ctrlW, ctrlH);
+		window.moveTo(Math.floor((screen.width - ctrlW) / 2), Math.floor((screen.height - ctrlH) / 2));
+	}
+	resizeWindow();
+
 	$(document).on('submit', "#loginform", function (e) {
 		$.ajax({
 			type: "POST",
