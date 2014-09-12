49e344ef8da4e0aa4ea90f377d8ea1d4b29145ce
Arseniy Maximov (Kern0) <localhost@kern0.ru>
2014-09-12 16:49:09 +0400

    Fix for deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ git pull origin production
 npm install
 
 # DB migration
-node db-migrations.js
+node ../scripts/db-migrations.js
 
 forever -a -l ../logs/forever.log -o ../logs/out.log -e ../logs/err.log restart gcapi.js
